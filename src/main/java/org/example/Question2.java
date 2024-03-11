a3fc043732bee828c9920f220f6baeca0f5c2383
rodi38 <rodrigorochaed1@gmail.com>
2024-03-11 19:45:52 -0300

    Question 2

---
+++ src/main/java/org/example/Question2.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        System.out.print("Digite um número para verificar se ele faz ou não parte da sequência fibonacci: ");
+        number = Integer.parseInt(sc.nextLine());
+        sc.close();
+        isFib(number);
+    }
+    public static void isFib(int x) {
+        int a = 0;
+        int b = 1;
+        if (x > 1) {
+            System.out.print(a + " " + b);
+        }
+        while (b < x) {
+            int tempNumber = b;
+            b = a+b;
+            a = tempNumber;
+            System.out.print(" " + b);
+        }
+
+        if (x == b) {
+            System.out.println("\nO número informado pertence a sequência fibonnaci.");
+            return;
+        }
+        System.out.println("\nO número informado NÃO pertence a sequência fibonnaci.");
+
+    }
+}
+
